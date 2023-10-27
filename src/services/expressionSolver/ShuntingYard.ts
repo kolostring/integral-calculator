e08ddce27b6c0ea41ec1c0e05e7c8f1914def874
kolostring <117553170+kolostring@users.noreply.github.com>
2023-10-27 01:08:20 -0400

    feat(Shunting Yard): exponentiation support. fix: bad stack and queue implementations. fix: bad operatorStack to queue concatenation.

--- src/services/expressionSolver/ShuntingYard.ts
+++ src/services/expressionSolver/ShuntingYard.ts
@@ -4,29 +4,37 @@ export default function ShuntingYard(expression: string[]) {
   const queue: string[] = [];
   const operatorStack: string[] = [];
 
-  for (const element of expression) {
-    if (element in operators) {
+  for (const token of expression) {
+    if (Object.keys(operators).includes(token)) {
       while (
         operatorStack.length > 0 &&
-        operatorStack[operatorStack.length - 1] != "(" &&
-        operators[operatorStack[operatorStack.length - 1]].precedence >=
-          operators[element].precedence
+        operatorStack[operatorStack.length - 1] !== "(" &&
+        ((operators[operatorStack[operatorStack.length - 1]]
+          .leftAssociativity &&
+          operators[operatorStack[operatorStack.length - 1]].precedence >=
+            operators[token].precedence) ||
+          (!operators[operatorStack[operatorStack.length - 1]]
+            .leftAssociativity &&
+            operators[operatorStack[operatorStack.length - 1]].precedence >
+              operators[token].precedence))
+      )
+        queue.push(operatorStack.pop()!);
+
+      operatorStack.push(token);
+    } else if (token === "(") {
+      operatorStack.push("(");
+    } else if (token === ")") {
+      while (
+        operatorStack.length > 0 &&
+        operatorStack[operatorStack.length - 1] !== "("
       ) {
         queue.push(operatorStack.pop()!);
       }
-
-      operatorStack.unshift(element);
-    } else if (element === "(") {
-      operatorStack.unshift("(");
-    } else if (element === ")") {
-      while (operatorStack[0] !== "(") {
-        queue.push(operatorStack.shift()!);
-      }
-      operatorStack.shift();
+      operatorStack.pop();
     } else {
-      queue.push(element);
+      queue.push(token);
     }
   }
 
-  return queue.concat(operatorStack);
+  return queue.concat(operatorStack.reverse());
 }
--- src/services/expressionSolver/operators.ts
+++ src/services/expressionSolver/operators.ts
@@ -2,6 +2,7 @@ interface Operator {
   [key: string]: {
     operation: (a: number, b: number) => number;
     precedence: number;
+    leftAssociativity: boolean;
   };
 }
 
@@ -9,18 +10,27 @@ export const operators: Operator = {
   "+": {
     operation: (a, b) => a + b,
     precedence: 0,
+    leftAssociativity: true,
   },
   "-": {
     operation: (a, b) => a - b,
     precedence: 0,
+    leftAssociativity: true,
   },
   "*": {
     operation: (a, b) => a * b,
     precedence: 1,
+    leftAssociativity: true,
   },
   "/": {
     operation: (a, b) => a / b,
     precedence: 1,
+    leftAssociativity: true,
+  },
+  "^": {
+    operation: (a, b) => Math.pow(a, b),
+    precedence: 2,
+    leftAssociativity: false,
   },
 };
 
