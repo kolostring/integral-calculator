e641883d84be59a06f69422ffd6d145ef2989975
kolostring <117553170+kolostring@users.noreply.github.com>
2023-11-24 19:02:32 -0500

    fix: SVGFunctionGrapher infinity support

--- src/components/SVGFunctionGrapher.tsx
+++ src/components/SVGFunctionGrapher.tsx
@@ -10,17 +10,14 @@ export default function SVGFunctionGrapher({
   graphProps,
   ...props
 }: Readonly<ISVGFunctionGrapher>) {
-  const maxY = Math.max.apply(null, functionPoints);
-  const minY = Math.min.apply(null, functionPoints);
   const deltaX = (to - from) / functionPoints.length;
 
-  const dimensions = Math.max(
-    Math.max(Math.abs(from) + to, 200),
-    Math.max(Math.abs(minY) + maxY, 200)
-  );
+  const dimensions = 200;
 
   const axesPathD = () => {
-    return `M 0 ${-dimensions+position.y} V ${dimensions+position.y} M ${-dimensions+position.x} 0 H ${dimensions+position.x} `;
+    return `M 0 ${-dimensions + position.y} V ${dimensions + position.y} M ${
+      -dimensions + position.x
+    } 0 H ${dimensions + position.x} `;
   };
 
   const graphPathD = () => {
@@ -28,7 +25,14 @@ export default function SVGFunctionGrapher({
 
     path += `M ${(deltaX + from) * scale} 0 `;
     functionPoints.forEach((y, index) => {
-      path += `L ${(index * deltaX + from) * scale} ${-y * scale} `;
+      path += `L ${(index * deltaX + from) * scale} `;
+      if (isFinite(y)) {
+        path += (-y * scale)+" ";
+      } else if (y === Infinity){
+        path += (99999999999)+" ";
+      } else {
+        path += (99999999999)+" ";
+      }
     });
 
     path += `V ${0}`;
