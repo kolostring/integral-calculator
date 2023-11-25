c5a25678e7e0b13367b62d20e98f0f1746404cdb
kolostring <117553170+kolostring@users.noreply.github.com>
2023-11-24 19:03:38 -0500

    fix: SVGGraphTransformator scale clamp

--- src/components/SVGGRaphTransformator.tsx
+++ src/components/SVGGRaphTransformator.tsx
@@ -10,15 +10,15 @@ export default function SVGGraphTransformator({
   className?: string
 }>) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [scale, setScale] = useState(1);
+  const [scale, setScale] = useState(10);
   const child = cloneElement(children, {scale: scale, position: position})
 
   const handleScale = (zoom: number) => {
-    setScale(scale + zoom);
+    setScale(Math.abs(scale + zoom));
   };
 
   const handlePosition = (x: number, y: number) => {
-    setPosition({ x: position.x + x, y: position.y + y });
+    setPosition({ x: position.x + x/1.6, y: position.y + y/1.6});
   };
 
   return (
