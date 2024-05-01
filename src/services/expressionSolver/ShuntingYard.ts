2e77a469156dd17611fbcd0ed78924e055206f88
kolostring <yoerodriguezjunior@gmail.com>
2024-05-01 17:22:43 -0400

    refactor: shunting yard improve readability

--- src/services/expressionSolver/ShuntingYard.ts
+++ src/services/expressionSolver/ShuntingYard.ts
@@ -4,47 +4,56 @@ import operators from "./operators";
 export default function ShuntingYard(expression: string[]) {
   const queue: string[] = [];
   const operatorStack: string[] = [];
-  for (const token of expression) {
+
+  expression.forEach((token)=>{
     if (token in operators) {
-      let lastOperator;
-
-      while (
-        operatorStack.length > 0 &&
-        (lastOperator = operatorStack[operatorStack.length - 1]) !== "(" &&
-        ((operators[lastOperator].leftAssociativity &&
-          operators[lastOperator].precedence >= operators[token].precedence) ||
-          (!operators[lastOperator].leftAssociativity &&
-            operators[lastOperator].precedence > operators[token].precedence))
-      )
-        queue.push(operatorStack.pop()!);
-
-      operatorStack.push(token);
+      parseOperator(token, operatorStack, queue);
     } else if (token === "(" || token in functions) {
       operatorStack.push(token);
     } else if (token === ")") {
-      try {
-        while (operatorStack[operatorStack.length - 1] !== "(") {
-          queue.push(operatorStack.pop()!);
-        }
-      } catch (e) {
-        throw new Error("Unpaired Parenthesis");
-      }
-      operatorStack.pop();
-
-      if (
-        operatorStack.length > 0 &&
-        operatorStack[operatorStack.length - 1] in functions
-      ) {
-        queue.push(operatorStack.pop()!);
-      }
+      parseClosedParenthesis(operatorStack, queue);
     } else {
       queue.push(token);
     }
-  }
+  })
 
   if (operatorStack.includes("(")) {
     throw new Error("Unpaired Parenthesis");
   }
 
   return queue.concat(operatorStack.reverse());
+}
+
+function parseOperator(token: string, operatorStack: string[], queue: string[]){
+  let lastOperator;
+
+  while (
+    operatorStack.length > 0 &&
+    (lastOperator = operatorStack[operatorStack.length - 1]) !== "(" &&
+    ((operators[lastOperator].leftAssociativity &&
+      operators[lastOperator].precedence >= operators[token].precedence) ||
+      (!operators[lastOperator].leftAssociativity &&
+        operators[lastOperator].precedence > operators[token].precedence))
+  )
+    queue.push(operatorStack.pop()!);
+
+  operatorStack.push(token);
+}
+
+function parseClosedParenthesis(operatorStack: string[], queue: string[]){
+  try {
+    while (operatorStack[operatorStack.length - 1] !== "(") {
+      queue.push(operatorStack.pop()!);
+    }
+  } catch (e) {
+    throw new Error("Unpaired Parenthesis");
+  }
+  operatorStack.pop();
+
+  if (
+    operatorStack.length > 0 &&
+    operatorStack[operatorStack.length - 1] in functions
+  ) {
+    queue.push(operatorStack.pop()!);
+  }
 }
