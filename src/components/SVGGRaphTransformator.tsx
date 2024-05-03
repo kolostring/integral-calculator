3734b87724cfdd8f1b10dfe2c66d3c3e69ae441c
kolostring <yoerodriguezjunior@gmail.com>
2024-05-03 07:18:38 -0400

    refactor: improve SVGGraphTransformator readability

--- src/components/SVGGRaphTransformator.tsx
+++ src/components/SVGGRaphTransformator.tsx
@@ -7,33 +7,39 @@ export default function SVGGraphTransformator({
   className,
 }: Readonly<{
   children: ReactElement<ISVGFunctionGrapher>;
-  className?: string
+  className?: string;
 }>) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [scale, setScale] = useState(10);
-  const child = cloneElement(children, {scale: scale, position: position})
+  const child = cloneElement(children, { scale: scale, position: position });
 
-  const handleScale = (zoom: number, relativeOrigin: {x: number, y: number}) => {
+  const handleScale = (
+    zoom: number,
+    relativeOrigin: { x: number; y: number }
+  ) => {
+    
     setScale(Math.max(Math.abs(scale + zoom), 5));
 
-    if(zoom !== 0){ 
+    if (zoom !== 0) {
       const zoomMult = zoom / scale;
-      setPosition({x: position.x + (position.x + relativeOrigin.x * 100) * zoomMult , y: position.y + (position.y + relativeOrigin.y * 100) * zoomMult});
+      setPosition({
+        x: position.x + (position.x + relativeOrigin.x * 100) * zoomMult,
+        y: position.y + (position.y + relativeOrigin.y * 100) * zoomMult,
+      });
     }
-
   };
 
   const handlePosition = (x: number, y: number) => {
-    setPosition({ x: position.x + x/1.6, y: position.y + y/1.6});
+    setPosition({ x: position.x + x / 1.6, y: position.y + y / 1.6 });
   };
 
   return (
-      <TransformationListener
-        onTranslate={handlePosition}
-        onZoom={handleScale}
-        className={className}
-      >
-        {child}
-      </TransformationListener>
+    <TransformationListener
+      onTranslate={handlePosition}
+      onZoom={handleScale}
+      className={className}
+    >
+      {child}
+    </TransformationListener>
   );
 }
