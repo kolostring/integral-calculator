b3e7455d28258794d79f52ae35e045c669a1bdf0
kolostring <yoerodriguezjunior@gmail.com>
2024-09-21 16:16:59 -0400

    feat: add support to more math functions

--- src/utils/constants/math.ts
+++ src/utils/constants/math.ts
@@ -9,6 +9,9 @@ export const mathFunctions: { [key: string]: (x: number) => number } = {
   tan: (x) => Math.tan(x),
   ln: (x) => Math.log(x),
   sqrt:(x) => Math.sqrt(x),
+  cbrt:(x) => Math.cbrt(x),
   log10: (x) => Math.log10(x),
   log2: (x) => Math.log2(x),
+  abs: (x) => Math.abs(x),
+  sign: (x) => Math.sign(x),
 };
