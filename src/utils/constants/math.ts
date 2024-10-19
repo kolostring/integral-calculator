13c071ff860ac82153a3c18e99061313715fdbdf
kolostring <yoerodriguezjunior@gmail.com>
2024-10-19 17:07:09 -0400

    feat: add more trigonometric functions

--- src/utils/constants/math.ts
+++ src/utils/constants/math.ts
@@ -4,14 +4,20 @@ export const mathConstants: { [key: string]: number } = {
 };
 
 export const mathFunctions: { [key: string]: (x: number) => number } = {
+  sqrt:(x) => Math.sqrt(x),
+  cbrt:(x) => Math.cbrt(x),
   sin: (x) => Math.sin(x),
   cos: (x) => Math.cos(x),
   tan: (x) => Math.tan(x),
+  asin: (x) => Math.asin(x),
+  acos: (x) => Math.acos(x),
+  atan: (x) => Math.atan(x),
   ln: (x) => Math.log(x),
-  sqrt:(x) => Math.sqrt(x),
-  cbrt:(x) => Math.cbrt(x),
   log10: (x) => Math.log10(x),
   log2: (x) => Math.log2(x),
   abs: (x) => Math.abs(x),
   sign: (x) => Math.sign(x),
+  round: (x) => Math.round(x),
+  floor: (x) => Math.floor(x),
+  ceil: (x) => Math.ceil(x),
 };
