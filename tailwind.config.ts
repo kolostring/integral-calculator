ab72bf5107d29fb13c06bfb8fb5e2e5dd06ccfec
kolostring <yoerodriguezjunior@gmail.com>
2024-10-19 17:08:13 -0400

    feat: add and configure new theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "selector",
   theme: {
-    extend: {},
-    fontFamily: {
-      'math': 'Latin Modern'
-    }
+    extend: {
+      fontFamily: {
+        display: ["Inter", "system-ui"], 
+        body: ["Inter", "system-ui"],
+      },
+      colors: {
+        text: "hsl(var(--text)/ <alpha-value>)",
+        background: "hsl(var(--background)/ <alpha-value>)",
+        primary: "hsl(var(--primary)/ <alpha-value>)",
+        neutral: "hsl(var(--neutral)/ <alpha-value>)",
+        secondary: "hsl(var(--secondary)/ <alpha-value>)",
+        accent: "hsl(var(--accent)/ <alpha-value>)",
+      },
+    },
   },
   plugins: [],
-
 };
