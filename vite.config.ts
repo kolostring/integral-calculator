7b8feacdf688c20c4fda533296c216c9aef58cb9
kolostring <yoerodriguezjunior@gmail.com>
2024-10-27 19:28:24 -0400

    build: add vite svgr for importing svg as react components

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   base: "/integral-calculator/",
   resolve: {
     alias: {
       "@": path.resolve("./src")
     }
-  }
+  },
 })
